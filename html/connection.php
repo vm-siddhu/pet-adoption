0a9e84dc4e53c702ffb227dced94c460241fa3f6
Venkata Mani Siddhu Padala <siddhupadala163@gmail.com>
2026-03-22 19:20:09 +0530

    changes

--- html/connection.php
+++ html/connection.php
@@ -3,13 +3,17 @@ $host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
 $port = 4000;
 $user = "35wvKbyfpupw6hZ.root";
 $pass = "JWdACyXeiZ6cN029";
-$db   = "test";
+$db   = "test"; // Make sure this matches the DB name you created (e.g., 'petadoption')
 
 $conn = mysqli_init();
+
+// 1. Prepare the SSL connection
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port)) {
+// 2. Connect using the MYSQLI_CLIENT_SSL flag (This fixes your error)
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+// Success!
 ?>
