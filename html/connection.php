1a96ed7589c372ccfbad5780e1e19ad1406d59e4
Venkata Mani Siddhu Padala <siddhupadala163@gmail.com>
2026-03-22 18:33:50 +0530

    connection

--- html/connection.php
+++ html/connection.php
@@ -1,14 +1,15 @@
 <?php
+$host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$port = 4000;
+$user = "35wvKbyfpupw6hZ.root";
+$pass = "JWdACyXeiZ6cN029";
+$db   = "test";
 
-$host = "sql204.infinityfree.com";     
-$user = "if0_41447937";         
-$password = "nuwUWLREJj";          
-$database = "if0_41447937_petadoption";
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-
-$conn = mysqli_connect($host, $user, $password, $database);
-
-if (!$conn) {
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port)) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
